55a2b0972edc1283b9faa18ea6bbc404e56f942f
maropra <mopradolce@gmail.com>
2021-05-23 15:16:14 -0300

    arreglo error msj en php

--- correo.php
+++ correo.php
@@ -7,9 +7,9 @@
     $msj = $_POST ['msj'];
 
     $header = 'Enviado desde la pagina de Tareas de Mar Opradolce';
-    $msjCompleto = $mensaje . '\nAtentamente: ' . $nombre;
+    $msjCompleto = $msj . '\nAtentamente: ' . $nombre;
 
     mail($destinatario, $asunto, $msjCompleto, $header);
     echo '<script>alert('Correo enviado exitosamente')</script>';
-    echo '<script>setTimeOut(\"location.href='index.html' \", 1000)</script>';
+    echo '<script> setTimeout(\"location.href='index.html' \", 1000)</script>';
 ?>
